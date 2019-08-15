7bfd6c3735a15d5abd5b5a51018f44c16141de59
Jeremy Amram <jeremy.j.amram@gmail.com>
2019-08-15 17:01:02 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,14 @@
+a = ["a","b","c","d"]
+n = ["e","f","g","h"]
+a.push("e")
+a.unshift("z")
+a.pop
+a.pop(2)
+a.shift
+a.shift(2)
+a.concat(b)
+a.insert(2,"z")
+a.uniq 
+a.flatten
+a.delete("b")
+a.delete_at(3)
